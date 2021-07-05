23be2ab01db77f78ed865d9242141d7c26c2f2d1
sonpham <soph3006@colorado.edu>
2021-07-05 15:10:52 -0500

    updating dissimilarity functions for numeric types

--- scripts/distances.py
+++ scripts/distances.py
@@ -28,8 +28,8 @@ def dissimilarity_nominal(dataset=None, p=None, m=None, weights=None):
                 dis_mat[i, j] = (p-m)/p
                 m = 0
         return dis_mat    
-    elif p and m:
-        return (p-m)/p
+    elif p != None and m != None:
+        return round((p-m)/p, 2)
 
 
 def similarity_nominal(dataset=None, p=None, m=None, weights=None):
@@ -50,8 +50,8 @@ def similarity_nominal(dataset=None, p=None, m=None, weights=None):
                                         weights=weights)
         sim_mat = np.subtract(1, dis_mat)
         return sim_mat
-    elif p and m:
-        return m/p
+    elif p != None and m != None:
+        return round(m/p, 2)
 
 
 def dissimilarity_binary(dataset=None, q=None, r=None, s=None, t=None, 
@@ -97,11 +97,11 @@ def dissimilarity_binary(dataset=None, q=None, r=None, s=None, t=None,
                 s = 0
                 t = 0
         return dis_mat    
-    elif q and r and s and t:
+    elif q != None and r != None and s != None and t != None:
         if symmetric:
-            return (r+s)/(q+r+s+t)
+            return round((r+s)/(q+r+s+t), 2)
         elif not symmetric:
-            return (r+s)/(q+r+s)
+            return round((r+s)/(q+r+s), 2)
 
 
 def similarity_binary(dataset=None, q=None, r=None, s=None, t=None, symmetric=True):
@@ -121,15 +121,52 @@ def similarity_binary(dataset=None, q=None, r=None, s=None, t=None, symmetric=Tr
                                    symmetric=symmetric)
         sim_mat = np.subtract(1, dis_mat)
         return sim_mat
-    elif q and r and s and t:
+    elif q != None and r != None and s != None and t != None:
         if symmetric:
-            return q/(q+r+s+t)
+            return round(q/(q+r+s+t), 2)
         else:
-            return q/(q+r+s)
+            return round(q/(q+r+s), 2)
 
 
-def manhattan_distance(x, y):
-    return sum(np.abs(a-b) for a, b in zip(x, y))
+def manhattan_distance(dataset=None, x=None, y=None):
+    """
+    :param dataset: two object dataset with numeric attributes
+    :param x: list of first object's numeric attributes
+    :param y: list of second object's numeric attributes
+    :return: manhattan distance
+    """
+    if dataset is not None:
+        x = dataset.iloc[0, :].tolist()
+        y = dataset.iloc[1, :].tolist()
+    return round(sum(np.abs(a-b) for a, b in zip(x, y)), 4)
+
+
+def euclidean_distance(dataset=None, x=None, y=None):
+    """
+    :param dataset: two object dataset with numeric attributes
+    :param x: list of first object's numeric attributes
+    :param y: list of second object's numeric attributes
+    :return: euclidean distance
+    """
+    if dataset is not None:
+        x = dataset.iloc[0, :].tolist()
+        y = dataset.iloc[1, :].tolist()
+    return round(np.sqrt(sum((a-b)**2 for a, b in zip(x ,y))), 4)
+
+
+def minkowski_distance(dataset=None, x=None, y=None, p_value=None):
+    """generalization of the euclidean and manhattan distances.
+    :param dataset: two object dataset with numeric attributes
+    :param x: list of first object's numeric attributes
+    :param y: list of second object's numeric attributes
+    :return: minkowski distance
+    """
+    if dataset is not None:
+        x = dataset.iloc[0, :].tolist()
+        y = dataset.iloc[1, :].tolist()
+    sum_val = sum(np.abs(a-b)**p_value for a, b in zip(x, y))
+
+    return np.round(sum_val**(1 / p_value), 4)
 
 
 def hamming_distance(s1, s2):
@@ -137,23 +174,9 @@ def hamming_distance(s1, s2):
     """
     if len(s1) != len(s2):
         raise ValueError("undefined for sequences of unequal length")
-    
     result = sum(ch1 != ch2 for ch1, ch2 in zip(s1, s2))
 
     return (len(s1) - result) / len(s1)
-
-
-def euclidean_distance(x, y):
-    return np.sqrt(sum((a-b)**2 for a, b in zip(x ,y)))
-
-
-def minkowski_distance(x, y, p_value):
-
-    sum_val = sum(np.abs(a-b)**p_value for a, b in zip(x, y))
-    root_val = 1 / p_value
-    result = np.round(sum_val**root_val, 3)
-
-    return result
 
 
 def cosine_similarity(x,y):
@@ -161,7 +184,7 @@ def cosine_similarity(x,y):
     sqrtx = round(np.sqrt(sum([a*a for a in x])), 3)
     sqrty = round(np.sqrt(sum([a*a for a in y])), 3)
     denom = sqrtx*sqrty
-    result = round(numerator/denom, 3)
+    result = round(numerator/denom, 4)
 
     return result
 
@@ -180,13 +203,13 @@ if __name__ == '__main__':
     df_nominal = df_mixed[['test1_nom']]
     # print(df_nominal)
     dis_mat = dissimilarity_nominal(dataset=df_nominal, p=None, m=None, weights=None)
-    print(dis_mat)
+    # print(dis_mat)
     # [[0. 1. 1. 0.]
     # [1. 0. 1. 1.]
     # [1. 1. 0. 1.]
     # [0. 1. 1. 0.]]
     sim_mat = similarity_nominal(dataset=df_nominal, p=None, m=None, weights=None)
-    print(sim_mat)
+    # print(sim_mat)
     # [[1. 0. 0. 1.]
     # [0. 1. 0. 0.]
     # [0. 0. 1. 0.]
@@ -210,44 +233,68 @@ if __name__ == '__main__':
                 df_binary.iloc[i, j] = 1
             elif df_binary.iloc[i, j] == 'N':
                 df_binary.iloc[i, j] = 0
-    print(df_binary)
+    # print(df_binary)
     df_binary_asym = df_binary[['fever', 'cough', 'test1', 'test2', 
                                 'test3', 'test4']]
     dis_mat = dissimilarity_binary(dataset=df_binary_asym, q=None, r=None, 
                                    s=None, t=None, symmetric=False)
-    print(dis_mat)
+    # print(dis_mat)
     # [[0.   0.67 0.33]
     # [0.67 0.   0.75]
     # [0.33 0.75 0.  ]]
     sim_mat = similarity_binary(dataset=df_binary_asym, q=None, r=None, 
                                 s=None, t=None, symmetric=False)
-    print(sim_mat)
+    # print(sim_mat)
     # [[1.   0.33 0.67]
     # [0.33 1.   0.25]
     # [0.67 0.25 1.  ]]
+    dis_val = dissimilarity_binary(dataset=None, q=1, r=1, 
+                                s=1, t=1, symmetric=False)
+    # print(dis_val) # 0.67
+    dis_val = dissimilarity_binary(dataset=None, q=1, r=1, 
+                                s=2, t=0, symmetric=False)
+    # print(dis_val) # 0.75
+
+
+    # numeric data dissimilarity
 
     # manhattan distance
-    result = manhattan_distance([10, 20, 10], [10, 20, 20])
-    # print(result)
+    result = manhattan_distance(x=[10, 20, 10], y=[10, 20, 20])
+    # print(result) # 10
+    dataset = pd.DataFrame(data=np.array([[10, 20, 10], [10, 20, 20]]), 
+                           columns=['a', 'b', 'c'])
+    result = manhattan_distance(dataset=dataset, x=None, y=None)
+    # print(result) # 10
+
+    # euclidean distance
+    result = euclidean_distance(x=[0, 3, 4, 5], y=[7, 6, 3, -1])
+    # print(result) # 9.7468
+    dataset = pd.DataFrame(data=np.array([[0, 3, 4, 5], [7, 6, 3, -1]]))
+    result = euclidean_distance(dataset=dataset, x=None, y=None)
+    # print(result) # 9.7468
+
+    # minkowski distance
+    result = minkowski_distance(x=[0, 3, 4, 5], y=[7, 6, 3, -1], p_value=3)
+    print(result) # 8.373
+    dataset = pd.DataFrame(data=np.array([[0, 3, 4, 5], [7, 6, 3, -1]]))
+    result = minkowski_distance(dataset=dataset, x=None, y=None, p_value=3)
+    print(result) # 8.373
+    exit()
 
     # hamming distance
     result = hamming_distance('CATCATCATCATCATCATCTTTTT',
                               'CATCATCTTCATCATCATCTTTTT')
-    # print(result)
+    print(result)
 
     # hamming distance 2
     result = hamming_distance('ATGCATCATCATCATCATCTTTTT',
                               'CATCATCTTCATCATCATCTTTTT')
-    # print(result)
+    print(result)
 
-    # euclidean distance
-    result = euclidean_distance([0, 3, 4, 5], [7, 6, 3, -1])
-    # print(result)
 
-    # minkowski distance
-    result = minkowski_distance([0, 3, 4, 5], [7, 6, 3, -1], 3)
-    # print(result)
+
+
 
     # minkowski distance
     result = cosine_similarity([5, 0, 3, 0, 2, 0, 0, 2, 0, 0], [3, 0, 2, 0, 1, 1, 0, 1, 0, 1])
-    # print(result)
+    print(result)
